ef4cd424071d27259e83f181bd0ad1cfc5bb3070
Elana Bell Bogdan <e.bell.bogdan@gmail.com>
2024-05-06 18:55:30 -0400

    Animate thought bubbles in & out

--- Assets/Scripts/ThoughtBubble.cs
+++ Assets/Scripts/ThoughtBubble.cs
@@ -1,13 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ThoughtBubble : MonoBehaviour
 {
     public string ThoughtText;
+    public float FadeSpeed = 2.5f;
+    public float InitialScale = 0.6f;
 
     [HideInInspector]
     public GameObject bubbleCanvas;
+
+    private bool fadeIn = false;
+    private bool fadeOut = false;
+
+    private CanvasGroup canvasGroup;
+    private GameObject animationParent;
+    private float currentScale = 0.6f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +26,54 @@ public class ThoughtBubble : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (bubbleCanvas == null) return;
+        if (fadeIn) {
+            if (canvasGroup.alpha < 1.0f) {
+                canvasGroup.alpha += FadeSpeed * Time.deltaTime;
+            }
+            if (currentScale < 1.0f) {
+                currentScale += FadeSpeed * Time.deltaTime * (1.0f - InitialScale);
+                animationParent.transform.localScale = new Vector3(currentScale, currentScale, currentScale);
+            }
+        } else if (fadeOut) {
+            if (canvasGroup.alpha > 0) {
+                canvasGroup.alpha -= FadeSpeed * Time.deltaTime;
+            }
+            if (currentScale > InitialScale) {
+                currentScale -= FadeSpeed * Time.deltaTime * (1.0f - InitialScale);
+                animationParent.transform.localScale = new Vector3(currentScale, currentScale, currentScale);
+            }
+        }
+    }
+
+    public void InstantiateBubble(GameObject bubblePrefab)
+    {
+        Collider myCollider = gameObject.GetComponent<Collider>(); 
+        Vector3 size = myCollider.bounds.size;
+        Vector3 position = myCollider.bounds.center;
+
+        bubbleCanvas = Instantiate(
+            bubblePrefab,
+            new Vector3 (position.x, position.y + size.y / 2 - 0.1f, position.z),
+            Quaternion.identity
+        );
+        TextMeshProUGUI bubbleText = bubbleCanvas.GetComponentInChildren<TextMeshProUGUI>();
+        bubbleText.SetText(ThoughtText);
+
+        animationParent = bubbleCanvas.transform.GetChild(0).gameObject;
+        animationParent.transform.localScale = new Vector3(currentScale, currentScale, currentScale);
+
+        canvasGroup = bubbleCanvas.GetComponentInChildren<CanvasGroup>();
+        canvasGroup.alpha = 0;
+    }
+
+    public void FadeIn() {
+        currentScale = InitialScale;
+        fadeIn = true;
+    }
+
+    public void FadeOut() {
+        fadeIn = false;
+        fadeOut = true;
     }
 }
--- Assets/Scripts/ThoughtManager.cs
+++ Assets/Scripts/ThoughtManager.cs
@@ -26,24 +26,15 @@ public class ThoughtManager : MonoBehaviour
         ThoughtBubble thought = targetObject.GetComponent<ThoughtBubble>();
 
         if (thought != null && thought.bubbleCanvas == null) {
-            Collider targetCollider = targetObject.GetComponent<Collider>(); 
-            Vector3 size = targetCollider.bounds.size;
-            Vector3 position = targetCollider.bounds.center;
-    
-            thought.bubbleCanvas = Instantiate(
-                BubbleCanvas,
-                new Vector3 (position.x + size.x / 2, position.y + size.y / 2 - 0.2f, position.z),
-                Quaternion.identity
-            );
-            TextMeshProUGUI bubbleText = thought.bubbleCanvas.GetComponentInChildren<TextMeshProUGUI>();
-            bubbleText.SetText(thought.ThoughtText);
+            thought.InstantiateBubble(BubbleCanvas);
         }
+        thought.FadeIn();
     }
+
     void OnTriggerExit(Collider collision) {
         ThoughtBubble thought = collision.gameObject.GetComponent<ThoughtBubble>();
         if (thought?.bubbleCanvas != null) {
-            Destroy(thought.bubbleCanvas);
-            thought.bubbleCanvas = null;
+            thought.FadeOut();
         }
     }
 }
